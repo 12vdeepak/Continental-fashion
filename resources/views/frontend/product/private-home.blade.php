1c92dc093adb9712410c2e0ae81b48f5a69049bf
quantumphp123 <rashad.quantumitinnovation@gmail.com>
2025-03-21 18:12:55 +0530

    Modal and mail

--- resources/views/frontend/product/private-home.blade.php
+++ resources/views/frontend/product/private-home.blade.php
@@ -19,7 +19,7 @@
                         <p id="description" class="mt-4 text-base sm:text-lg md:text-xl w-full">
                             Lorem ipsum dolor sit amet consectetur. At ultrices libero et congue mauris sed nisl.
                         </p>
-                        <a href="">
+                        <a href="{{ route('frontend.all.product') }}">
                             <button id="shopBtn"
                                 class="mt-6 px-4 sm:px-6 py-2 sm:py-3 bg-[#54114C] hover:bg-purple-700 text-white text-base sm:text-lg font-semibold rounded-lg transition duration-300">
                                 Shop Now →
